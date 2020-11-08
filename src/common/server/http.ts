04eba00ec2f02a4deeb65f06b5d52491c3d0fec2
Karol Szymanowski <szyma.karol@gmail.com>
2020-11-08 11:03:00 +0100

    feat(config): add config's routes to the app

--- src/common/server/http.ts
+++ src/common/server/http.ts
@@ -8,6 +8,9 @@ import * as yaml from 'yamljs';
 import * as cors from 'cors';
 import * as helmet from 'helmet';
 import { Logger } from '../logger/logger';
+import { CustomError, ErrorType, InternalError, NotFoundError } from '../errors/errors';
+import { Router } from 'express';
+import * as bodyParser from 'body-parser';
 
 export interface CorsOptions {
   origin: string;
@@ -24,8 +27,9 @@ export class HttpServer {
     this.logger.info('Shutting down server...');
   }
 
+  // TODO: implement
   private async onHealthCheck() {
-    this.logger.info('Health check');
+    this.logger.debug('Health check');
   }
 
   setupDocs(docsPath: string) {
@@ -40,18 +44,41 @@ export class HttpServer {
   }
 
   setupCors(opts?: CorsOptions) {
-    this.logger.info('Setting up cors');
+    this.logger.debug('Setting up cors');
     this.app.use(cors(opts));
   }
 
   setupMiddleware() {
-    this.logger.info('Setting up middlewares');
+    this.logger.debug('Setting up middlewares');
     this.app.use(helmet());
+    this.app.use(bodyParser.json());
+  }
+
+  loadRoutes(routes: { prefix: string; router: Router }[]) {
+    routes.forEach((route) => {
+      this.logger.debug(`Loading router /${route.prefix}`);
+      this.app.use('/' + route.prefix, route.router);
+    });
   }
 
   start() {
-    this.app.get('/', (req, res) => {
-      res.send('Hello world!');
+    this.app.use('**', (req, res, next) => next(new NotFoundError("Page doesn't exist")));
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.app.use((err, req, res, next) => {
+      const error: CustomError = !!err.type ? err : new InternalError(err);
+      switch (error.type) {
+        case ErrorType.IncorrectInput:
+          this.logger.debug(`(${req.method} ${req.path}) ${error.errorDetails}`);
+          return res.status(400).json({ error: error.type, details: error.errorDetails });
+        case ErrorType.NotFound:
+          this.logger.debug(`(${req.method} ${req.path}) ${error.errorDetails}`);
+          return res.status(404).json({ error: error.type, details: error.errorDetails });
+        default:
+          this.logger.error(`(${req.method} ${req.path}) ${error.stack || error.errorDetails}`);
+          return res.status(500).json({ error: error.type });
+      }
     });
 
     const server = http.createServer(this.app);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { HttpServer } from './common/server/http';
 import { Configs } from './common/config/config';
 import { validateOrReject } from 'class-validator';
 
+import { configRouter } from './config/ports/http';
+
 const config = new Configs();
 const winstonLogger = new WinstonLogger(config.logLevel);
 
@@ -14,6 +16,7 @@ async function init() {
   httpServer.setupCors();
   httpServer.setupMiddleware();
   httpServer.setupDocs('./api/openapi');
+  httpServer.loadRoutes([{ prefix: 'config', router: configRouter }]);
   httpServer.start();
 }
 
